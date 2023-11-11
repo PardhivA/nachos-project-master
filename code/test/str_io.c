6a387dcf055d7d2bb24df740536e034d79e6755c
PardhivA <pardhiv19nov@gmail.com>
2023-11-11 16:09:33 +0530

    sleep and priority

---
+++ code/test/str_io.c
@@ -0,0 +1,16 @@
+/* str_io.c
+ *	Simple program to test whether the systemcall interface works.
+ *
+ *	Just do a syscall that read a string and print it
+ *
+ */
+
+#include "syscall.h"
+char a[256];
+int main() {
+    // PrintUCString("String length: (<= 255):\n");
+    // ReadString(a, ReadNum());
+    int pid = Exec("nstr");
+    Join(pid);
+   while(1){ PrintUCString("hekkidekki \n");}
+}
